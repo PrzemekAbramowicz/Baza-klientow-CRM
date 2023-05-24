c2e852d432ce1ef5c1111401dcd3bfafac7ba32c
PrzemekAbramowicz <abramowicz.dev@gmail.com>
2023-05-24 20:26:03 +0200

    first configuration

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const { clientRouter } = require('./routers/client');
+
+const app = express();
+
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.static('public'));
+app.use(express.json());
+app.engine(
+	'.hbs',
+	hbs.engine({
+		extname: '.hbs',
+		// helpers: handlebarsHelpers,
+	})
+);
+app.set('view engine', '.hbs');
+
+app.use('/client', clientRouter);
+
+app.listen(3000, 'localhost', () => {
+	console.log(`Server started on port 3000`);
+});
