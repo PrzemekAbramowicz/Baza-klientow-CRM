1702c4caeced059811068811751e6819884f8b4a
PrzemekAbramowicz <abramowicz.dev@gmail.com>
2023-05-24 22:37:31 +0200

    updated client.js for CRUD operations

--- routers/client.js
+++ routers/client.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const { db } = require('../utlis/db');
 
 const clientRouter = express.Router();
 
 clientRouter
-    .get('/', (req, res) => {
-      res.send('OK')
-    })
+	.get('/', (req, res) => {
+		res.render('client/list-all', {
+			clients: db.getAll(),
+		});
+	})
+	.get('/:id', (req, res) => {
+		res.render('client/one', {
+			client: db.getOne(req.params.id),
+		});
+	})
+	.post('/', (req, res) => {
+		res.send('Dodaj!');
+	})
+	.put('/:id', (req, res) => {
+		res.send('Zmodyfikuj!');
+	})
+	.delete('/:id', (req, res) => {
+		res.send('Usuń!');
+	});
 
 module.exports = {
 	clientRouter,
