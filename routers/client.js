54af630a2c1559408b98fa37ce99a25034df0482
PrzemekAbramowicz <abramowicz.dev@gmail.com>
2023-05-26 00:39:21 +0200

    adding possibility to edit customer details

--- routers/client.js
+++ routers/client.js
@@ -22,7 +22,11 @@ clientRouter
 		});
 	})
 	.put('/:id', (req, res) => {
-		res.send('Zmodyfikuj!');
+		db.update(req.params.id, req.body);
+		res.render('client/modified', {
+			name: req.body.name,
+			id: req.params.id,
+		});
 	})
 	.delete('/:id', (req, res) => {
 		db.delete(req.params.id);
@@ -30,6 +34,11 @@ clientRouter
 	})
 	.get('/form/add', (req, res) => {
 		res.render('client/forms/add');
+	})
+	.get('/form/edit/:id', (req, res) => {
+		res.render('client/forms/edit', {
+			client: db.getOne(req.params.id),
+		});
 	});
 
 module.exports = {
